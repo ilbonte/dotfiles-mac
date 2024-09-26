a3a1743f9d52ec5b5b6ba9f92b0b875fcf3115ee
Alessio Libardi <58233289+alessio-libardi@users.noreply.github.com>
2024-09-26 20:14:03 +0000

    test: fix

--- shell/bash/config.sh
+++ shell/bash/config.sh
@@ -10,7 +10,4 @@ grep -qxF "$SHELL" /etc/shells || echo "$SHELL" >>/etc/shells
 echo "Changing default shell to $SHELL"
 chsh -s "$SHELL"
 
-echo "Changing current shell to $SHELL"
-exec "$SHELL"
-
 echo "Shell configured!"
