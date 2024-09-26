5c2060edd433466fd4af024f552f84614187ef82
Alessio Libardi <58233289+alessio-libardi@users.noreply.github.com>
2024-09-26 21:48:03 +0000

    fix: testing

--- os/mac/config.sh
+++ os/mac/config.sh
@@ -2,10 +2,13 @@
 
 echo "Configuring MacOS..."
 
-echo "Installing Homebrew..."
-NONINTERACTIVE=1 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! command -v brew &>/dev/null; then
+    echo "Installing Homebrew..."
+    NONINTERACTIVE=1 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
 
 echo "Configuring applications & libraries..."
-brew bundle
+brew bundle --file=./os/mac/Brewfile
 
 echo "MacOS configured!"
