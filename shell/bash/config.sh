ef017737d328e789846fae19aca16d2aedabab5f
Alessio Libardi <58233289+alessio-libardi@users.noreply.github.com>
2024-09-26 20:11:52 +0000

    fix: config shell

--- shell/bash/config.sh
+++ shell/bash/config.sh
@@ -2,20 +2,15 @@
 
 echo "Configuring shell..."
 
-NEW_SHELL="/opt/homebrew/bin/bash"
+# Define the shell path
+SHELL="/opt/homebrew/bin/bash"
 
-# Check if the new shell is already in /etc/shells
-if ! grep -Fxq "$NEW_SHELL" /etc/shells; then
-    echo "Adding $NEW_SHELL to /etc/shells"
-    echo "$NEW_SHELL" | sudo tee -a /etc/shells > /dev/null
-else
-    echo "$NEW_SHELL is already in /etc/shells"
-fi
+grep -qxF "$SHELL" /etc/shells || echo "$SHELL" >>/etc/shells
 
-echo "Changing default shell to $NEW_SHELL"
-chsh -s "$NEW_SHELL"
+echo "Changing default shell to $SHELL"
+chsh -s "$SHELL"
 
-echo "Changing current shell to $NEW_SHELL"
-exec "$NEW_SHELL"
+echo "Changing current shell to $SHELL"
+exec "$SHELL"
 
 echo "Shell configured!"
