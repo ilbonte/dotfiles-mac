6ac05337e23075f5d8a4cae865a69bb670bcd5e0
Alessio Libardi <58233289+alessio-libardi@users.noreply.github.com>
2024-09-26 20:19:50 +0000

    test: fix

--- shell/bash/config.sh
+++ shell/bash/config.sh
@@ -5,7 +5,7 @@ echo "Configuring shell..."
 # Define the shell path
 SHELL="/opt/homebrew/bin/bash"
 
-grep -qxF "$SHELL" /etc/shells || echo "$SHELL" >>/etc/shells
+echo "$SHELL" | sudo tee -a /etc/shells
 
 echo "Changing default shell to $SHELL"
 chsh -s "$SHELL"
