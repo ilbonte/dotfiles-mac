9a5029b9e760bebfffcf8585a785abb090d2c9e9
Alessio Libardi <58233289+alessio-libardi@users.noreply.github.com>
2024-10-01 19:39:05 +0000

    test

--- shell/bash/config.sh
+++ shell/bash/config.sh
@@ -6,14 +6,14 @@ echo "Configuring libraries..."
 brew bundle --file=./shell/bash/Brewfile
 
 # Define the shell path
-SHELL_PATH="/opt/homebrew/bin/bash"
+# SHELL_PATH="/opt/homebrew/bin/bash"
 
-grep -qxF "$SHELL_PATH" /etc/shells || echo "$SHELL_PATH" | sudo tee -a /etc/shells
+# grep -qxF "$SHELL_PATH" /etc/shells || echo "$SHELL_PATH" | sudo tee -a /etc/shells
 
-echo "Changing default shell to $SHELL_PATH"
-if [ "$(ps -p $$ -o 'comm=')" != "bash" ]; then
-  sudo chsh -s "$SHELL_PATH"
-  exec "$SHELL_PATH"
-fi
+# echo "Changing default shell to $SHELL_PATH"
+# if [ "$(ps -p $$ -o 'comm=')" != "bash" ]; then
+#   sudo chsh -s "$SHELL_PATH"
+#   exec "$SHELL_PATH"
+# fi
 
 echo "Shell configured!"
