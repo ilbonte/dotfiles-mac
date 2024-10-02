b3be259fcca4ba7830afa4ae614e0804d013dc20
Alessio Libardi <58233289+alessio-libardi@users.noreply.github.com>
2024-10-02 14:11:31 +0000

    test

--- lib/os/config.sh
+++ lib/os/config.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 source ~/.bashrc
-
 echo "Installing package manager..."
-IS_PACKAGE_MANAGER_INSTALLED=$(command -v brew 2>&1 >/dev/null)
-if ! $IS_PACKAGE_MANAGER_INSTALLED; then
+if ! command -v brew &>/dev/null; then
   NONINTERACTIVE=1 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
@@ -12,19 +10,19 @@ echo "Detecting OS..."
 
 case $(uname -s) in
 Darwin)
-  if ! $IS_PACKAGE_MANAGER_INSTALLED; then
+  if ! command -v brew &>/dev/null; then
     MACOS_PACKAGE_MANAGER_PATH="/opt/homebrew/bin/brew"
     echo "eval \"\$(${MACOS_PACKAGE_MANAGER_PATH} shellenv)\"" >> ~/.bashrc
-    eval "$MACOS_PACKAGE_MANAGER_PATH shellenv"
+    eval "$(${MACOS_PACKAGE_MANAGER_PATH} shellenv)"
     source ~/.bashrc
   fi
   bash ./lib/os/mac/config.sh
   ;;
 Linux)
-  if ! $IS_PACKAGE_MANAGER_INSTALLED; then
+  if ! command -v brew &>/dev/null; then
     LINUX_PACKAGE_MANAGER_PATH="/home/linuxbrew/.linuxbrew/bin/brew"
     echo "eval \"\$(${LINUX_PACKAGE_MANAGER_PATH} shellenv)\"" >> ~/.bashrc
-    eval "$LINUX_PACKAGE_MANAGER_PATH shellenv"
+    eval "$(${LINUX_PACKAGE_MANAGER_PATH} shellenv)"
     source ~/.bashrc
   fi
   bash ./lib/os/linux/config.sh
